48d5071dae70a7ca1dfe06e7694ca8cceebb2808
Light <lightman.chang@gmail.com>
2025-06-18 09:32:34 +0800

    Remove inline GA snippet from HTML pages

--- js/ga.js
+++ js/ga.js
@@ -1,21 +1,46 @@
 // Google Analytics helper functions
 let gaInitialized = false;
 
+function getUserId() {
+    const KEY = 'vuko_user_id';
+    try {
+        let id = localStorage.getItem(KEY);
+        if (!id) {
+            if (window.crypto && crypto.randomUUID) {
+                id = crypto.randomUUID();
+            } else {
+                id = Date.now().toString(36) + Math.random().toString(36).substr(2);
+            }
+            localStorage.setItem(KEY, id);
+        }
+        return id;
+    } catch (e) {
+        return 'uid_' + Date.now();
+    }
+}
+
 function initGoogleAnalytics() {
     if (!CONFIG.GOOGLE_ANALYTICS.ENABLE_TRACKING || !CONFIG.GOOGLE_ANALYTICS.GA_ID || gaInitialized) {
         return;
     }
 
-    const script1 = document.createElement('script');
-    script1.async = true;
-    script1.src = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.GOOGLE_ANALYTICS.GA_ID}`;
-    document.head.appendChild(script1);
+    if (!window.gtag) {
+        const script1 = document.createElement('script');
+        script1.async = true;
+        script1.src = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.GOOGLE_ANALYTICS.GA_ID}`;
+        document.head.appendChild(script1);
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    window.gtag = gtag;
-    gtag('js', new Date());
-    gtag('config', CONFIG.GOOGLE_ANALYTICS.GA_ID);
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        window.gtag = gtag;
+        gtag('js', new Date());
+        gtag('config', CONFIG.GOOGLE_ANALYTICS.GA_ID);
+    }
+
+    const lang = (navigator.language || '').toLowerCase().split('-')[0];
+    const uid = getUserId();
+    gtag('set', {'user_id': uid});
+    gtag('set', 'user_properties', { 'lang': lang });
 
     gaInitialized = true;
 }
